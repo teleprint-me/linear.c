7b057c2d66f86d2d51b7125eca77f46758e3f1de
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-29 23:32:50 -0400

    tests(matrix): Add stub for matrix tests, todo

---
+++ tests/test_linear_matrix.c
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file tests/test_linear_matrix.c
+ *
+ * @note keep fixtures and related tests as simple as reasonably possible.
+ *       The simpler, the better.
+ */
+
+#include "logger.h"
+#include "vector.h"
+
+int main(void) {
+    return 0; // @todo
+}
