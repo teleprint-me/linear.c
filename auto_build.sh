448704c95cb6cc75100345e7ef3aacd61490f795
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-29 23:45:13 -0400

    auto(refactor): Simplify the build process
    - Use env instead of directly calling bash
    - Call ctest directly without changing directories

--- auto_build.sh
+++ auto_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Define the build directory
 BUILD_DIR=build
@@ -11,5 +11,4 @@ cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Debug
 cmake --build ${BUILD_DIR} --config Debug -j 16
 
 # Run tests
-cd ${BUILD_DIR}
-ctest --rerun-failed --output-on-failure
+ctest --rerun-failed --output-on-failure --test-dir build
