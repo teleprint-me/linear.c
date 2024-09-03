12589abe6fc52e01217481ff7767e99b20ad2b2e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-03 00:18:19 -0400

    auto(refactor): Use nproc to get the number of available cpu's

--- auto_build.sh
+++ auto_build.sh
@@ -8,7 +8,7 @@ rm -rf ${BUILD_DIR}
 
 # Run cmake and build
 cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Debug
-cmake --build ${BUILD_DIR} --config Debug -j 16
+cmake --build ${BUILD_DIR} --config Debug -j $(nproc)
 
 # Run tests
 ctest --rerun-failed --output-on-failure --test-dir build
