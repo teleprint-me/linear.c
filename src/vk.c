d174974d09fb4e0005623609603fcc06aa275437
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-04 13:56:54 -0400

    vulkan(patch): Add missing inclusions, add missing comments, and update version info

--- src/vk.c
+++ src/vk.c
@@ -10,23 +10,28 @@
  * Only pure C is used with minimal dependencies on external libraries.
  */
 
+#include "vk.h"
+
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <vulkan/vulkan.h>
 
+// Helper function to create a application info object
 VkApplicationInfo vk_linear_application_info(const char* pApplicationName) {
     VkApplicationInfo pApplicationInfo = {
         .sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pApplicationName   = pApplicationName,
-        .applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0),
+        .applicationVersion = VK_MAKE_API_VERSION(0, 1, 3, 0),
         .pEngineName        = "linear",
-        .engineVersion      = VK_MAKE_API_VERSION(0, 1, 0, 0),
-        .apiVersion         = VK_API_VERSION_1_0,
+        .engineVersion      = VK_MAKE_API_VERSION(0, 1, 3, 0),
+        .apiVersion         = VK_API_VERSION_1_3,
     };
     return pApplicationInfo;
 }
 
+// Helper function to create a vulkan instance object
 VkResult vk_linear_create_instance(
     const VkApplicationInfo* pApplicationInfo, VkInstance* pInstance
 ) {
