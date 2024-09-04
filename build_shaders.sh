65b262167edf9efbe444a717ae9299d86b280216
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-03 20:15:01 -0400

    shaders(build): Add comments, cleanup, and simplify path and file name references

--- build_shaders.sh
+++ build_shaders.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 SHADER_DIR="shaders"
-OUTPUT_DIR="shaders/compiled"
+OUTPUT_DIR="${SHADER_DIR}/compiled"
 
-# Clean output directory if it does exist
+# Clean output directory if it exists
 rm -rf "$OUTPUT_DIR"
-# Create output directory if it doesn't exist
+# Create output directory
 mkdir -p "$OUTPUT_DIR"
 
-# Find all .comp files and compile them
+# Compile each shader
 for shader in "${SHADER_DIR}"/*; do
+    # Skip directories
     if [ -d "$shader" ]; then continue; fi
+    
+    # Extract the file name, omitting the file path
     shader_name=$(basename "$shader")
+    # Substitute the file extension, from .comp to .spv
+    shader_name="${shader_name%.*}.spv"
+
+    # Compile the shader
     echo "Compiling ${shader}..."
-    glslang -V "${shader}" -o "$OUTPUT_DIR/${shader_name%.*}.spv"
+    glslang -V "$shader" -o "${OUTPUT_DIR}/${shader_name}" || {
+        echo "Error: Failed to compile ${shader}"
+        exit 1
+    }
 done
 
 echo "Shader compilation completed."
