ff7637d59d9a61254dafde062f52aec09c58d2fb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-05 19:47:28 -0400

    examples(vector): Refactor simple vector example to use lehmer

--- examples/vector_simple.c
+++ examples/vector_simple.c
@@ -4,62 +4,74 @@
  * @file examples/vector_simple.c
  *
  * Manual Build:
+ *     # binary output
  *     gcc -o examples/vector_simple \
- *         mods/logger/src/logger.c src/vector.c \
+ *         # dependencies
+ *         mods/logger/src/logger.c \
+ *         mods/logger/src/lehmer.c \
+ *         # source
+ *         src/vector.c \
+ *         # this file
  *         examples/vector_simple.c
+ *         # link libraries
  *         -lpthread -lm \
- *         -I./include -I./mods/logger/include
+ *         # required include paths
+ *         -I./include \
+ *         -I./mods/logger/include \
+ *         -I./mods/lehmer/include
  */
 
+#include "lehmer.h"
 #include "vector.h"
 
 #include <stdio.h>
 
-#define MODULUS    2147483647
-#define MULTIPLIER 48271
-
-int lehmer_generate_modulo(int seed) {
-    // Scale the seed and return the remainder.
-    return (MULTIPLIER * seed) % MODULUS;
-}
-
-// NOTE: Normalization "generates" the pseudo random number
-float lehmer_seed_normalize(int seed) {
-    // Normalize the seed as a ratio of the modulus.
-    return (float) seed / (float) MODULUS;
-}
-
-void lehmer_initialize(int seed, vector_t* vector) {
+// @brief initialize a vector with randomly generated values
+void vector_initialize(lehmer_state_t* state, vector_t* vector) {
     for (size_t i = 0; i < vector->columns; i++) {
-        seed            = lehmer_generate_modulo(seed);
-        float n         = lehmer_seed_normalize(seed);
-        vector->data[i] = n;
+        // typecast from double to float because lehmer returns double
+        float n         = (float) lehmer_random_modulo(state);
+        vector->data[i] = n; // this must be a float value
     }
 }
 
 int main(void) {
-    int    seed    = 1337;
-    size_t columns = 4;
+    // initialize the rng state
+    size_t          streams = 2;
+    int64_t         seed    = 1337;
+    lehmer_state_t* state   = lehmer_state_create(streams, seed);
 
-    vector_t* a = vector_create(columns);
-    vector_t* b = vector_create(columns);
+    // allocate memory to vectors
+    size_t    columns = 4;
+    vector_t* a       = vector_create(columns);
+    vector_t* b       = vector_create(columns);
 
     // @note Do not allocate memory to the result.
-    vector_t* result;
+    vector_t* result; // ops allocate memory for us
 
-    lehmer_initialize(seed, a);
-    lehmer_initialize(seed + 1, b);
+    // initialize vector a
+    lehmer_state_select(state, 0); // use primary stream
+    vector_initialize(state, a);
+
+    // initialize vector b
+    lehmer_state_select(state, 1); // use secondary stream
+    vector_initialize(state, b);
 
     // @note Result is automatically allocated memory
     result = vector_vector_add(a, b);
 
+    // output the results for vector addition
     for (size_t i = 0; i < result->columns; i++) {
         printf("result->data[%zu] = %f\n", i, result->data[i]);
     }
 
+    // free all vectors
     vector_free(a);
     vector_free(b);
     vector_free(result);
+
+    // free the rng state
+    lehmer_state_free(state);
 
     return 0;
 }
