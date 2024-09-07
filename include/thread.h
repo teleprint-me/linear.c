1f0e113dddf71f770f77069642466cbb9fa514b3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-06 20:52:01 -0400

    thread(refactor): Add revised draft for multi-threading support

--- include/thread.h
+++ include/thread.h
@@ -24,26 +24,34 @@
     #define LINEAR_THREAD_COUNT 4
 #endif // LINEAR_THREAD_COUNT
 
-typedef enum LinearData {
-    TYPE_VECTOR,
-    TYPE_MATRIX,
-    TYPE_TENSOR
-} linear_data_t;
+// There are 3 types of structures:
+//     vector_t, matrix_t, and tensor_t
+// We need to avoid potential future conflicts with signal processing
+// and disassociate the name schema from referencing data types to
+// mitigate potential confusion. Declaring this as some vague linear
+// struct type is probably not any better, but I don't know what else
+// to name this at the current moment and this is better than nothing.
+// Consider whether this is an appropriate naming schema and tread carefully.
+typedef enum LinearStructure {
+    LINEAR_VECTOR,
+    LINEAR_MATRIX,
+    LINEAR_TENSOR
+} linear_struct_t;
 
 // @note Don't know what data types are being used in advance
 typedef struct {
     void*  a; ///< Pointer to the first operand (vector, matrix, or tensor).
     void*  b; ///< Pointer to the second operand (vector, matrix, or tensor).
-    void*  result;      ///< Pointer to the result data structure.
-    size_t begin;       ///< Starting index for the thread to operate.
-    size_t end;         ///< Ending index for the thread to operate.
-    linear_data_t type; ///< Type of data (vector, matrix, tensor).
+    void*  result;        ///< Pointer to the result data structure.
+    size_t begin;         ///< Starting index for the thread to operate.
+    size_t end;           ///< Ending index for the thread to operate.
+    linear_struct_t type; ///< Type of struct (e.g. vector, matrix, tensor).
     float (*operation)(float, float); ///< Pointer to the operation function.
 } linear_thread_t;
 
 // @note Could pass the number of threads to the structure?
 //       Doesn't feel right, though.
-linear_thread_t* linear_thread_create();
+linear_thread_t* linear_thread_create(/* todo: void for now */ void);
 void             linear_thread_free(linear_thread_t* thread);
 
 // Initialize a thread pool (optional)
