ba4dece3a237b8383f7cda9ee048a418ecbaa982
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-09 03:21:58 -0400

    scalar(refactor): Apply dynamic data type handling for scalar based operations

--- include/scalar.h
+++ include/scalar.h
@@ -11,9 +11,16 @@
 #ifndef LINEAR_SCALAR_H
 #define LINEAR_SCALAR_H
 
-#include "thread.h"
-
 // Scalar operations
+
+// Define the linear data type
+typedef enum LinearData {
+    LINEAR_FLOAT,
+    LINEAR_INT,
+} linear_data_t;
+
+// not sure if this should be here or in thread.h?
+typedef void* (*linear_operation_t)(void*, void*, linear_data_t type);
 
 /**
  * @brief Perform arithmetic operations
@@ -30,21 +37,21 @@
 /**
  * @brief Add two floating-point numbers
  */
-float scalar_add(float x, float y);
+void* scalar_add(void* x, void* y, linear_data_t type);
 
 /**
  * @brief Subtract two floating-point numbers
  */
-float scalar_subtract(float x, float y);
+void* scalar_subtract(void* x, void* y, linear_data_t type);
 
 /**
  * @brief Multiply two floating-point numbers
  */
-float scalar_multiply(float x, float y);
+void* scalar_multiply(void* x, void* y, linear_data_t type);
 
 /**
  * @brief Divide two floating-point numbers
  */
-float scalar_divide(float x, float y);
+void* scalar_divide(void* x, void* y, linear_data_t type);
 
 #endif // LINEAR_SCALAR_H
