6977edcfb4d4bcfae4caab7a1f96df30c5b04f8b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-08 20:36:11 -0400

    thread(refactor): Replace size_t with uint32_t for consistency and fix docstrings

--- include/thread.h
+++ include/thread.h
@@ -3,13 +3,21 @@
  *
  * @file include/thread.h
  *
- * @brief A simple and easy-to-use Thread API in C
+ * @brief A simple and easy-to-use Thread Pool API in C
  *
- * This header file provides an implementation for basic mathematical
- * operations on N-dimensional tensors in N-dimensional space within a threaded
- * pool.
+ * This header file provides functions for creating, managing, and freeing a
+ * thread pool to perform mathematical operations on N-dimensional tensors in
+ * parallel using pthreads library.
  *
  * Only pure C is used with minimal dependencies on external libraries.
+ *
+ * @note linear_data_t is reserved for signal processing.
+ * @ref See teleprint-me/fixed-point for more information
+ * https://github.com/teleprint-me/fixed-point.git
+ *
+ * @note The thread could should always be configurable.
+ * @ref See GNU C Extensions for more information
+ * https://gcc.gnu.org/onlinedocs/gcc-12.2.0/gcc/C-Extensions.html
  */
 
 #ifndef LINEAR_THREAD_H
@@ -21,9 +29,7 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
-// @note The thread could should always be configurable
-// @ref See GNU C Extensions for more information
-// https://gcc.gnu.org/onlinedocs/gcc-12.2.0/gcc/C-Extensions.html
+// Define the maximum number of threads if not provided
 #ifndef LINEAR_THREAD_COUNT
     #ifdef __GNUC__
         #define LINEAR_THREAD_COUNT get_nprocs_conf()
@@ -32,38 +38,30 @@
     #endif // __GNUC__
 #endif     // LINEAR_THREAD_COUNT
 
-// There are 3 types of structures:
-//     vector_t, matrix_t, and tensor_t
-// We need to avoid potential future conflicts with signal processing
-// and disassociate the name schema from referencing data types to
-// mitigate potential confusion. Declaring this as some vague linear
-// struct type is probably not any better, but I don't know what else
-// to name this at the current moment and this is better than nothing.
-// Consider whether this is an appropriate naming schema and tread carefully.
+// Define the types of Linear structures
 typedef enum LinearStructure {
-    LINEAR_VECTOR,
-    LINEAR_MATRIX,
-    LINEAR_TENSOR
+    LINEAR_VECTOR, ///< vector_t
+    LINEAR_MATRIX, ///< matrix_t
+    LINEAR_TENSOR  ///< tensor_t
 } linear_struct_t;
 
-// @note Don't know what data types are being used in advance
-typedef struct {
-    void*  a; ///< Pointer to the first operand (vector, matrix, or tensor).
-    void*  b; ///< Pointer to the second operand (vector, matrix, or tensor).
-    void*  result;        ///< Pointer to the result data structure.
-    size_t begin;         ///< Starting index for the thread to operate.
-    size_t end;           ///< Ending index for the thread to operate.
-    linear_struct_t type; ///< Type of struct (e.g. vector, matrix, tensor).
+// Define the thread structure
+typedef struct LinearThread {
+    void*    a; ///< Pointer to the first operand (vector, matrix, or tensor).
+    void*    b; ///< Pointer to the second operand (vector, matrix, or tensor).
+    void*    result;      ///< Pointer to the result data structure.
+    uint32_t begin;       ///< Starting index for the thread to operate.
+    uint32_t end;         ///< Ending index for the thread to operate.
+    linear_struct_t type; ///< Type of struct (e.g., vector, matrix, tensor).
     float (*operation)(float, float); ///< Pointer to the operation function.
 } linear_thread_t;
 
-// @note Could pass the number of threads to the structure?
-//       Doesn't feel right, though.
+// Function prototypes
 linear_thread_t* linear_thread_create(uint32_t num_threads);
 void             linear_thread_free(linear_thread_t* thread);
 
 // Initialize a thread pool (optional)
-void linear_thread_pool_create(size_t num_threads);
+void linear_thread_pool_create(uint32_t num_threads);
 void linear_thread_pool_free(void);
 
 // Function to perform an operation in parallel
